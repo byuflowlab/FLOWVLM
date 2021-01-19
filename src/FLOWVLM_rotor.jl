db514f0c14fba26cb062425c1850c5eeed2aade3
Judd Mehr <juddmehr@gmail.com>
2021-01-19 09:06:19 -0700

    remove tip loss correction

--- src/FLOWVLM_rotor.jl
+++ src/FLOWVLM_rotor.jl
@@ -264,7 +264,7 @@ function solvefromCCBlade(self::Rotor, Vinf, RPM, rho::FWrap; t::FWrap=0.0,
                             include_comps::Bool=true, return_performance::Bool=false,
                             Vref=nothing, sound_spd=nothing, Uinds=nothing,
                             _lookuptable::Bool=false, _Vinds=nothing,
-                            tiploss_correction=true, AR_to_360extrap=true)
+                            tiploss_correction=false, AR_to_360extrap=true)
 
   setVinf(self, Vinf)
   setRPM(self, RPM)
@@ -1117,7 +1117,7 @@ function calc_distributedloads(self::Rotor, Vinf, RPM, rho::FWrap;
                                 Uinds=nothing,
                                 sound_spd=nothing,
                                 _lookuptable::Bool=false, _Vinds=nothing,
-                                tiploss_correction::Bool=true,
+                                tiploss_correction::Bool=false,
                                 AR_to_360extrap = true)
   data = Array{FArrWrap}[]
   if include_comps
@@ -1179,8 +1179,8 @@ function calc_distributedloads(self::Rotor, Vinf, RPM, rho::FWrap;
       advance_ratio = sqrt(vfs[1]^2 + vfs[2]^2 + vfs[3]^2) / ((RPM/60)*(2*self.rotorR))
 
       Np, Tp, uvec, vvec, gamma, cn, ct, cl, cd = _calc_distributedloads_lookuptable(occbrotor,
-                                                        occbinflow, turbine_flag, advance_ratio;
-                                                        tiploss_correction=tiploss_correction)
+                            occbinflow, turbine_flag, advance_ratio;
+                            tiploss_correction=tiploss_correction)
       push!(gammas, gamma)
       zeroarr = zeros(size(Np))
       ccbouputs = ccb.Outputs(Np, Tp, zeroarr, zeroarr, uvec, vvec,
@@ -1970,7 +1970,7 @@ function _calc_distributedloads_lookuptable(ccbrotor::OCCBRotor,
                                             ccbinflow::OCCBInflow,
                                             turbine_flag::Bool,
                                             advance_ratio::Float64;
-                                            tiploss_correction::Bool=true)
+                                            tiploss_correction::Bool=false)
 
     # check if propeller
     swapsign = turbine_flag ? 1 : -1
