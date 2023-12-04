1e8995e0dd17ebcaacaea6c1076f586ff5811cd7
Ryan Anderson <rymanderson@gmail.com>
2023-12-04 14:05:30 -0700

    potential bug fix

--- src/FLOWVLM_solver.jl
+++ src/FLOWVLM_solver.jl
@@ -95,7 +95,7 @@ function solve(HSs::Array{Array{Any,1},1}, Vinfs::Array{FArrWrap,1};
                 t::FWrap=0.0,
                 vortexsheet=nothing, extraVinf=nothing, extraVinfArgs...)
 
-  @show eltype(eltype(eltype(HSs))) HSs[1][1][1] HSs[1][1]
+  @show eltype(eltype(eltype(HSs))) HSs[1][1][1] HSs[1]
   TF = promote_type(typeof(t),eltype(eltype(HSs)),eltype(eltype(Vinfs)))
   n = size(HSs)[1]            # Number of horseshoes
   G = zeros(TF, n, n)      # Geometry matrix
