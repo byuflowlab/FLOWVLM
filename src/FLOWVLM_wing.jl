cb73c566d8ccc147fa1151b991452e32e2c96195
Ryan Anderson <rymanderson@gmail.com>
2023-12-04 14:01:05 -0700

    potential bug fix

--- src/FLOWVLM_wing.jl
+++ src/FLOWVLM_wing.jl
@@ -430,10 +430,8 @@ function _calculateHSs(self::Wing{TF}; t::FWrap=0.0, extraVinf=nothing, extraVin
     Gamma = "Gamma" in keys(self.sol) ? self.sol["Gamma"][i] : nothing
 
     HS = [Ap, A, B, Bp, CP, infDA, infDB, Gamma]
-    @show typeof(HS) typeof(HSs) HS HSs
     push!(HSs, HS)
   end
-  @show typeof(self._HSs) typeof(HSs)
   self._HSs = HSs
 end
 
